f292a55aa86f5744db4cd2a3685213f608449361
Tanuj Singh Kushwah <tanujsinghkushwah@gmail.com>
2019-06-15 14:25:14 +0530

    Fetching data

--- lib/main.dart
+++ lib/main.dart
@@ -1,198 +1,208 @@
 import 'package:flutter/material.dart';
-
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'constants.dart';
 
-void main() => runApp(new MyApp());
+Future<Article> fetchPost() async {
+  final response =
+  await http.get('https://www.datadiscuss.com/ghost/api/v2/content/posts/?key=477da0c526d10b1f39c02185a7');
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: new MyHomePage(),
+  if (response.statusCode == 200) {
+    // If server returns an OK response, parse the JSON
+    return Article.fromJson(json.decode(response.body));
+  } else {
+    // If that response was not OK, throw an error.
+    throw Exception('Failed to load post');
+  }
+}
+
+class Article{
+  final String id;
+  final String title;
+  final String image;
+
+  Article({this.id, this.title, this.image});
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+        title: json['posts'][0]['title'],
+        id: json['posts'][0]['id'],
+        image: json['posts'][0]['feature_image']
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
-  Animation cardAnimation, delayedCardAnimation, fabButtonanim, infoAnimation;
-  AnimationController controller;
-
+class MyHomePage extends StatelessWidget {
+  final Future<Article> title;
+  MyHomePage({Key key, this.title}) : super(key: key);
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    controller =
-        AnimationController(duration: Duration(seconds: 2), vsync: this);
-
-    cardAnimation = Tween(begin: 0.0, end: -0.025).animate(
-        CurvedAnimation(curve: Curves.fastOutSlowIn, parent: controller));
-
-    delayedCardAnimation = Tween(begin: 0.0, end: -0.05).animate(
-        CurvedAnimation(
-            curve: Interval(0.5, 1.0, curve: Curves.fastOutSlowIn),
-            parent: controller));
-
-    fabButtonanim = Tween(begin: 1.0, end: -0.0008).animate(CurvedAnimation(
-        curve: Interval(0.8, 1.0, curve: Curves.fastOutSlowIn),
-        parent: controller));
-
-    infoAnimation = Tween(begin: 0.0, end: 0.025).animate(CurvedAnimation(
-        curve: Interval(0.7, 1.0, curve: Curves.fastOutSlowIn),
-        parent: controller));
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+          leading: IconButton(
+            icon: new Image.asset('assets/datadiscuss.png'),
+            color: Colors.black,
+            onPressed: () {},
+          ),
+          title: new Text('DataDiscuss',
+              style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black)),
+          actions: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(right: 8.0),
+              child: PopupMenuButton<String>(
+                icon: new Icon(Icons.more_vert, color: Colors.black),
+                itemBuilder: (BuildContext context){
+                  return Constants.choices.map((String choice){
+                    return PopupMenuItem<String>(
+                        value: choice,
+                        child: Text(choice)
+                    );
+                  }).toList();
+                },
+              ),
+            )
+          ],
+        ),
+        body: Center(
+          child: SwipeList())
+    );
   }
+}
+
+class SwipeList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return ListItemWidget();
+  }
+}
+
+class ListItemWidget extends State<SwipeList> {
+  List items = getDummyList();
 
   @override
   Widget build(BuildContext context) {
-    final devHeight = MediaQuery.of(context).size.height;
-    controller.forward();
-    return new AnimatedBuilder(
-        animation: controller,
-        builder: (BuildContext context, Widget child) {
-          return Scaffold(
-            appBar: new AppBar(
-              backgroundColor: Colors.white,
-              elevation: 0.0,
-              leading: IconButton(
-                icon: new Image.asset('assets/datadiscuss.png'),
-                color: Colors.black,
-                onPressed: () {},
-              ),
-              title: new Text('DataDiscuss',
-                  style: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black)),
-              actions: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(right: 8.0),
-                  child: PopupMenuButton<String>(
-                      icon: new Icon(Icons.more_vert, color: Colors.black),
-                      itemBuilder: (BuildContext context){
-                        return Constants.choices.map((String choice){
-                          return PopupMenuItem<String>(
-                            value: choice,
-                            child: Text(choice)
-                          );
-                        }).toList();
-                      },
-                  ),
-                )
-              ],
-            ),
-            body: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                    padding: EdgeInsets.fromLTRB(30.0, 30.0, 20.0, 40.0),
-                    child: Stack(
-                      overflow: Overflow.visible,
-                      children: <Widget>[
-                        Positioned(
-                          left: 20.0,
-                          child: Container(
-                            transform: Matrix4.translationValues(0.0, delayedCardAnimation.value * devHeight, 0.0),
-                            width: 260.0,
-                            height: 400.0,
-                            decoration: BoxDecoration(
-                                color: Colors.amber,
-                                borderRadius: BorderRadius.circular(10.0)),
-                          ),
-                        ),
-                        Positioned(
+            return Container(
+                child: FutureBuilder<Article>(
+                future: fetchPost(), //sets the getQuote method as the expected Future
+    builder: (context, snapshot) {
+    if (snapshot.hasData) {
+      return Center(
+          child: ListView.builder(
 
-                          left: 10.0,
-                          child: Container(
-                            transform: Matrix4.translationValues(0.0, cardAnimation.value * devHeight, 0.0),
-                            width: 280.0,
-                            height: 400.0,
-                            decoration: BoxDecoration(
-                                color: Colors.blue,
-                                borderRadius: BorderRadius.circular(10.0)),
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              return Dismissible(
+                key: Key(items[index]),
+                background: Container(
+                  alignment: AlignmentDirectional.centerEnd,
+                  color: Colors.red,
+                  child: Icon(
+                    Icons.delete,
+                    color: Colors.white,
+                  ),
+                ),
+                onDismissed: (direction) {
+                  setState(() {
+                    items.removeAt(index);
+                  });
+                },
+                direction: DismissDirection.endToStart,
+                child: Card(
+                  elevation: 5,
+                  child: Container(
+                    height: 300.0,
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          height: 200.0,
+                          width: 70.0,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(5),
+                                  topLeft: Radius.circular(5)
+                              ),
+                              image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(snapshot.data.image)
+                              )
                           ),
                         ),
                         Container(
-                          width: 300.0,
-                          height: 400.0,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10.0),
-                              image: DecorationImage(
-                                  image: AssetImage('assets/girls.jpeg'),
-                                  fit: BoxFit.cover)),
-                        ),
-                        Positioned(
-                          top: 320.0,
-                          left: 15.0,
-                          child: Container(
-                            transform: Matrix4.translationValues(0.0, infoAnimation.value * devHeight, 0.0),
-                            width: 270.0,
-                            height: 90.0,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10.0),
-                                color: Colors.white,
-                                boxShadow: [
-                                  BoxShadow(
-                                      blurRadius: 1.0,
-                                      color: Colors.black12,
-                                      spreadRadius: 2.0)
-                                ]),
-                            child: Container(
-                              padding: EdgeInsets.all(7.0),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: <Widget>[
-                                  Row(
-                                    children: <Widget>[
-                                      Text(
-                                        'Data Discuss',
-                                        style: TextStyle(
-                                            fontFamily: 'Montserrat',
-                                            fontSize: 20.0),
-                                      ),
-                                      SizedBox(width: 4.0),
-                                      Image.asset(
-                                        'assets/simbolo.png',
-                                        height: 20.0,
-                                        width: 20.0,
-                                      ),
-                                      SizedBox(width: 110.0),
-                                      Text(
-                                        '',
-                                        style: TextStyle(
-                                            fontFamily: 'Montserrat',
-                                            fontSize: 20.0,
-                                            color: Colors.grey),
-                                      ),
-                                    ],
+                          height: 100,
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(10, 2, 0, 0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  items[index],
+
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.fromLTRB(0, 3, 0, 3),
+                                  child: Container(
+                                    width: 200,
+                                    decoration: BoxDecoration(
+                                        border: Border.all(color: Colors.teal),
+                                        borderRadius: BorderRadius.all(Radius.circular(10))
+                                    ),
+                                    child: Text(snapshot.data.id,textAlign: TextAlign.center,),
                                   ),
-                                  SizedBox(height: 9.0),
-                                  Row(
-                                    children: <Widget>[
-                                      Text(
-                                        'Machine Learning',
-                                        style: TextStyle(
-                                            fontFamily: 'Montserrat',
-                                            fontSize: 15.0,
-                                            color: Colors.grey),
-                                      )
-                                    ],
-                                  )
-                                ],
-                              ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.fromLTRB(0, 5, 0, 2),
+                                  child: Container(
+                                    width: 260,
+                                    child: Text(snapshot.data.title,style: TextStyle(
+                                        fontSize: 15,
+                                        color: Color.fromARGB(255, 48, 48, 54)
+
+                                    ),),
+                                  ),
+                                )
+                              ],
                             ),
                           ),
                         )
                       ],
-                    )),
-              ],
-            ),
-          );
-        });
+                    ),
+                  ),
+                ),
+              );
+            },
+          )
+      );
+    }})
+ );
+          }
+        }
+
+
+   List getDummyList() {
+    List list = List.generate(10, (i) {
+      return "Item ${i + 1}";
+    });
+    return list;
   }
-}
